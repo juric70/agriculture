47028df8010fd9040b9f6eaf0a44e9b8a9116ede
ZvonkoZlo <zvonko.zlopasa@fsre.sum.ba>
2023-04-27 00:39:46 +0200

    C+V

---
+++ Agrokultura/Models/City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Agrokultura.Models;
+
+public partial class City
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? PostalCode { get; set; }
+
+    public int CountryId { get; set; }
+
+    public virtual Country Country { get; set; } = null!;
+
+    public virtual ICollection<Person> People { get; set; } = new List<Person>();
+}
---
+++ Agrokultura/Models/Person.cs
@@ -0,0 +1,44 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Agrokultura.Models;
+
+public partial class Person
+{
+    public int Id { get; set; }
+
+    [Required]
+    [Display(Name = "Puno ime")]
+
+    public string FullName { get; set; } = null!;
+
+    public string PhoneNumber { get; set; } = null!;
+
+    public string? Email { get; set; }
+
+    public string Adress { get; set; } = null!;
+
+    public int? CityId { get; set; }
+
+    public int? RoleId { get; set; }
+
+    public virtual ICollection<ChorePerson> ChorePeople { get; set; } = new List<ChorePerson>();
+
+    public virtual City? City { get; set; }
+
+    public virtual ICollection<Contract> ContractBeneficiaries { get; set; } = new List<Contract>();
+
+    public virtual ICollection<Contract> ContractProviders { get; set; } = new List<Contract>();
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+
+    public virtual ICollection<Plant> Plants { get; set; } = new List<Plant>();
+
+    public virtual ICollection<Plot> Plots { get; set; } = new List<Plot>();
+
+    public virtual Role? Role { get; set; }
+}
